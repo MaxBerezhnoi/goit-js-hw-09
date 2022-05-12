15aa9c289587e9d9d97afcc156f243592d2dc3b1
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-05-13 00:04:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+/*если кнопка старт неактивна - фон меняет  рэндомно цвет*/
+function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        }
 
+const btnStart = document.querySelector("button[data-start]");
+    btnStart.addEventListener("click", bgcRandom); 
+    
+const btnStop = document.querySelector("button[data-stop]");
+    btnStop.addEventListener("click", stopColor);
+    
+let timerId = null;
+
+
+function bgcRandom(evt) {
+    
+    timerId = setInterval(() => {
+        const body = document.querySelector("body");
+        body.style.backgroundColor = getRandomHexColor();
+        
+    }, 1000);
+}
+
+function stopColor(evt) {
+    clearInterval(timerId);
+}
