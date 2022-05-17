7832bf146c88ca0bf2987171e425ce32eca56e44
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-05-18 00:33:53 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,12 +5,55 @@ import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 /*const flatpickr = require("flatpickr");*/
+const body = document.querySelector("body");
+body.style.display = "flex";
+body.style.flexDirection = "column";
+body.style.alignItems = "center";
+body.style.alignContent = "space-around";
+
+const timer = document.querySelector(".timer");
+timer.style.display = "flex";
+timer.style.alignItems = "center";
+timer.style.justifyContent = "space-around";
+timer.style.width = "600px";
+timer.style.heigth = "150px";
+timer.style.border = "3px solid red";
+timer.style.fontSize = "20px";
+timer.style.color = "blue";
+timer.style.backgroundColor = "yellow";
+timer.style.padding = "2px";
+timer.style.marginTop = "10px";
+
+
+const fields = document.querySelectorAll(".field");
+for (const field of fields) {
+    field.style.border = "2px solid #3cff00";
+    field.style.borderRadius = "25%";
+    field.style.padding = "2px";
+}
+
+const values = document.querySelectorAll(".value");
+for (const value of values) {
+    value.style.fontStyle = "italic";
+    value.style.fontWeight = "700";
+    value.style.color = "#ff17ff";
+}
+
+
+
+const d = document.querySelector(".value[data-days]");
+const h = document.querySelector(".value[data-hours]");
+const m = document.querySelector(".value[data-minutes]");
+const s = document.querySelector(".value[data-seconds]");
+
+
 
 const btnStart = document.querySelector("button[data-start]");
 btnStart.setAttribute("disabled", true);
 btnStart.addEventListener("click", startTimer);
 
 let timerId = 0;
+
 
 
 
@@ -29,45 +72,58 @@ const options = {
             btnStart.removeAttribute("disabled");
             console.log("Выбранное время:");
             console.log(selectedDates[0]);
-            console.log(selectedDates[0].getTime());
-            console.log(options.defaultDate.getTime());
-            const ms = Number(selectedDates[0].getTime() - options.defaultDate.getTime());
-            console.log(ms);
-            return ms;
+            options.selDate = selectedDates[0].getTime();
+            
         }
         
   },
 };
 
+flatpickr(input, options);
 
 function startTimer(evt) {
+   
+    
     btnStart.setAttribute("disabled", true);
     timerId = setInterval(() => {
-        function convertMs(ms) {
-  // Number of milliseconds per unit of time
     
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-        console.log("timer starting!");
-console.log(convertMs());
+            convertMs();
+            console.log(convertMs());
+        
     }, 1000);
+
+
+    };
+
+
+function convertMs() {
+    let ms = Math.floor(options.selDate - Date.now());
+    console.log(ms);
+    if (ms <= 0) {
+        clearInterval(timerId);
+        return;
+    }
+    else {
+        // Number of milliseconds per unit of time 
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+            
+        // Remaining days
+        const days = Math.floor(ms / day);
+        d.textContent = days;
+        // Remaining hours
+        const hours = Math.floor((ms % day) / hour);
+        h.textContent = hours;
+        // Remaining minutes
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        m.textContent = minutes;
+        // Remaining seconds
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+        s.textContent = seconds;
+
+        return { days, hours, minutes, seconds };
+    }
     
-    ;
-
-};
-
-flatpickr(input, options);
+}
